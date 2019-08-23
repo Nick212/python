7856345bb5288708ddd92d32dba06de0d578a62f
Nícolas Alexandre de Carvalho <nicolas@enube.me>
2019-08-22 22:49:44 -0300

    leaning while and looping, ok

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,21 +4,25 @@ print("*********************************")
 
 numero_secreto = 42
 
-chute_str = input("Digite o seu número: ")
+total_tentativas = 3
 
-print("Você digitou ", chute_str)
+while (total_tentativas > 0):
+    print("Rodada: ", total_tentativas)
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou ", chute_str)
+    
+    # converter string para int
+    chute = int(chute_str)
 
-# converter string para int
-chute  = int(chute_str)
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
-
-if(acertou):
-    print("Você acertou")
-else:
-    if(maior):
-        print("Você erro! o seu chute foi maior do que o número secreto")
-    elif(menor):
-        print("Você erro! o seu chute foi menor do que o número secreto")
+    if(acertou):
+        print("Você acertou")
+    else:
+        if(maior):
+            print("Você erro! o seu chute foi maior do que o número secreto")
+        elif(menor):
+            print("Você erro! o seu chute foi menor do que o número secreto")
+    total_tentativas = total_tentativas - 1
